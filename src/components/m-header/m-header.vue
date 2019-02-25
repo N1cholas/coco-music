ed1819dd4312bb5334409fc474aa5bc171dda49d
N1cholas <z875346836@icloud.com>
2019-02-25 10:35:57 +0800

    impl header

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="m-header">
-    m-header
-  </div>
+  <header class="m-header">
+    <div class="logo"></div>
+    <h1 class="title">coco-music</h1>
+  </header>
 </template>
 <script>
 export default {
@@ -9,5 +10,24 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-
+  @import '~common/stylus/variable'
+  @import '~common/stylus/mixin'
+  .m-header
+    text-align center
+    font-size 0
+    height 44px
+    .logo
+      bg-image('logo')
+      width 30px
+      height 32px
+      display inline-block
+      margin-top 6px
+      margin-right 9px
+      vertical-align top
+      background-size cover
+    .title
+      display inline-block
+      font-size $font-size-large
+      vertical-align top
+      line-height 44px
 </style>
